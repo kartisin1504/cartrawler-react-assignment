880f8b05882a44e087d01f26ae7ba28c234b6c0b
kartisin1504 <100683471+kartisin1504@users.noreply.github.com>
2025-09-13 11:55:40 +0100

    created webpack copy for move assets

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,1 +1,16 @@
-const {merge}=require('webpack-merge');const common=require('./webpack.common.js');module.exports=merge(common,{mode:'production',devtool:'source-map'})
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'public/assets'), to: 'assets' },
+      ],
+    }),
+  ],
+});
