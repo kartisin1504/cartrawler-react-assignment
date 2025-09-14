5df2f85b13bac52aaad00dad09c63ac1e815b34b
kartisin1504 <100683471+kartisin1504@users.noreply.github.com>
2025-09-14 08:28:03 +0100

    updated vendor name fix

--- src/components/CarCard/CarTitle.tsx
+++ src/components/CarCard/CarTitle.tsx
@@ -5,7 +5,7 @@ export function CarTitle({ vendor, name }: { vendor: string; name: string }) {
   return (
     <div className="card-title">
       <img className="vendor-logo" src={vendorLogo(vendor)} alt={vendor} title={vendor} />
-      <h3>{name}</h3>
+      <small className="vendor-name">{name}</small>
     </div>
   )
 }
